ac50bc83cf7f2399eca9b07042de4e559a02db82
PerfTestOps <perftestanalyzer@gmail.com>
2025-07-03 21:24:32 +0530

    Update Transaction_Analyzer.py

--- Transaction_Analyzer.py
+++ Transaction_Analyzer.py
@@ -192,6 +192,6 @@ def transaction_analysis_page():
             mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
         )
     else:
-        st.info("Please upload an Excel file (.xlsx) to begin.")
+        st.info("")
 
 
